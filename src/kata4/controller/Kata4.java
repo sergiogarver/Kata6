9b12e9c0aa29a97b7e3bc4b099be25f40524898e
usuario <usuario@10.22.148.52>
2017-10-27 12:05:00 +0100

    Versión 2

--- src/kata4/controller/Kata4.java
+++ src/kata4/controller/Kata4.java
@@ -1,4 +1,5 @@
 package kata4.controller;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import kata4.model.Histogram;
@@ -7,13 +8,33 @@ import kata4.view.MailHistogramBuider;
 import kata4.view.MailListReader;
 
 public class Kata4 {
+    private List<String> mailList;
+    private String filename;
+    private Histogram<String> histogram;
+    
     public static void main(String[] args) throws IOException {
-        String filename = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata4\\emailsFile.txt";
-        List<String> mailList = MailListReader.read(filename);
-        Histogram<String> histogram = MailHistogramBuider.build(mailList);
+        Kata4 kata4 = new Kata4();
+        kata4.execute();      
+    }
+    public void execute() throws FileNotFoundException, IOException{
+        input();
+        process();
+        output();
+    }
+    
+    public void input() throws FileNotFoundException, IOException{
+        filename = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata4\\emailsFile.txt";
+        mailList = MailListReader.read(filename);
+    
+    }
+    
+    public void process(){
+        histogram = MailHistogramBuider.build(mailList);
+    }
+    
+    public void output(){
         HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-        histoDisplay.execute();        
-
+        histoDisplay.execute();      
     }
 
     
